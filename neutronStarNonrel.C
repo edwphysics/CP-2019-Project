256c711ccc18f0f29042d99b17f2e2e158395ecd
Edwin Pérez <edwpes8a@gmail.com>
2022-01-06 21:53:02 -0500

    Add files via upload
    RK4 solvers of the State Equation of a white dwarf star.

---
+++ neutronStarNonrel.C
@@ -0,0 +1,176 @@
+//IMPLEMENTATION OF THE RUNGE-KUTTA 4-TH ORDER
+//NEUTRON STAR - Nonrelativistic Case gamma=5/3
+//y = pressure, z = mass
+
+/*
+	COMMENTS: 
+	ADDITIONS: GR Factors, R0, k, Gamma, R rescaling, slide Mass to avoid divergence
+	ISSUES: 
+*/
+
+//GLOBAL CONSTANTS AND VARIABLES
+int n = 8000, l = 1;
+double alpha = 1, beta = 0.7636, k = 1.914; //DE Constants
+double Delta = 1e-30;			 			//Sliding constant to avoid r=0 divergence
+double Gamma = 5./3.;						//Polytripe parameter
+double R0 = 1.47;							//one-half of Schwartzschild radius
+
+double flag = false;
+  
+double f(double y[], double z[], double t, int j){
+	//GENERALIZED VELOCITY
+	double v[l];
+	if (flag == false)
+	{
+		printf("%e\t", (alpha*pow(y[0],1./Gamma)*z[0]/pow(t,2)));
+		printf("%e\t", (1+pow(k,1./Gamma)*pow(y[0],1-1./Gamma)));
+		printf("%e\t", (1+beta*pow(k,1./Gamma)*pow(t,2)*y[0]/z[0]));
+		printf("%e\n", (1-2*R0*z[0]/t));
+	}
+	v[0] = -(alpha*pow(y[0],1./Gamma)*z[0]/pow(t,2))*(1+pow(k,1./Gamma)*pow(y[0],1-1./Gamma))*(1+beta*pow(k,1./Gamma)*pow(t,2)*y[0]/z[0])/(1-2*R0*z[0]/t);
+	return v[j];
+}
+
+double g(double y[], double z[], double t, int j){
+	//GENERALIZED VELOCITY
+	double v[l];
+	v[0] = beta*pow(t,2)*pow(y[0],1./Gamma);
+	return v[j];
+}
+
+double RungeKuttaG(double y[], double z[], double t, double dt, int j){
+	double d1[l], d2[l], d3[l], d4[l];
+	double c1[l], c2[l], c3[l], c4[l];
+	double D1[l], D2[l], D3[l], D4[l];
+	double C1[l], C2[l], C3[l], C4[l];
+
+	d1[j] = f(y, z, t, j);
+	c1[j] = g(y, z, t, j);
+
+	D2[j] = y[j] + dt*d1[j]/2;
+	C2[j] = z[j] + dt*c1[j]/2;
+	d2[j] = f(D2, C2, t+dt/2, j);
+	c2[j] = g(D2, C2, t+dt/2, j);
+
+	D3[j] = y[j] + dt*d2[j]/2;
+	C3[j] = z[j] + dt*c2[j]/2;
+	d3[j] = f(D3, C3, t+dt/2, j);
+	c3[j] = g(D3, C3, t+dt/2, j);
+
+	D4[j] = y[j] + dt*d3[j];
+	C4[j] = z[j] + dt*c3[j];
+	d4[j] = f(D4, C4, t + dt, j);
+	c4[j] = g(D4, C4, t + dt, j);
+
+	d1[j] = y[j] + dt*(d1[j]+2*(d2[j]+d3[j])+d4[j])/6;
+	return d1[j];
+}
+
+double RungeKuttaF(double y[], double z[], double t, double dt, int j){
+	double d1[l], d2[l], d3[l], d4[l];
+	double c1[l], c2[l], c3[l], c4[l];
+	double D2[l], D3[l], D4[l];
+	double C2[l], C3[l], C4[l];
+
+	d1[j] = f(y, z, t, j);
+	c1[j] = g(y, z, t, j);
+
+	D2[j] = y[j] + dt*d1[j]/2;
+	C2[j] = z[j] + dt*c1[j]/2;
+	d2[j] = f(D2, C2, t+dt/2, j);
+	c2[j] = g(D2, C2, t+dt/2, j);
+
+	D3[j] = y[j] + dt*d2[j]/2;
+	C3[j] = z[j] + dt*c2[j]/2;
+	d3[j] = f(D3, C3, t+dt/2, j);
+	c3[j] = g(D3, C3, t+dt/2, j);
+
+	D4[j] = y[j] + dt*d3[j];
+	C4[j] = z[j] + dt*c3[j];
+	d4[j] = f(D4, C4, t + dt, j);
+	c4[j] = g(D4, C4, t + dt, j);
+
+	c1[j] = z[j] + dt*(c1[j]+2*(c2[j]+c3[j])+c4[j])/6;
+	return c1[j];
+}
+
+void principal(double p0){
+	//FLAG & CRITERION TO DISPLAY THE RESULT
+	int signal;
+	bool signalb = false;
+	double criterion = p0*1e-5;
+
+	//VARIABLES
+	double y1[n+1], y[l];
+	double z1[n+1], z[l];
+
+	//SET UP TIME STEP AND INITIAL VALUES
+	double dt = 15./n;
+	y1[0] = p0;
+	y[0] = p0;
+
+	z1[0] = 0.;
+	z[0] = 0.;
+
+	double T[n+1];
+
+	//PERFORM THE 4TH-ORDER RUNGE-KUTTA INTEGRATION
+	for (int i = 0; i < n; ++i)
+	{
+		double t = dt*i + Delta;
+		T[i] = t;
+
+		for (int j = 0; j < l; ++j)
+		{
+			y[j] = RungeKuttaG(y, z, t, dt, j);
+			z[j] = RungeKuttaF(y, z ,t, dt, j);
+		}
+
+		y1[i+1] = y[0];
+		z1[i+1] = z[0];		
+
+		//FIND THE FINAL RADIUS AND MASS VALUE
+		if ((y[i+1] <= criterion) && (signalb == false) && (i>10))
+		{
+			signal = i+1;
+			signalb = true;
+		}
+
+		if (i>10)
+		{
+			flag = true;
+		}
+	}
+
+	//PRINT RESULT
+	//printf("%.1e\t%lf\t%lf\n", p0, T[signal], z[signal]);
+
+	//GRAPHS
+	TCanvas *c1 = new TCanvas();
+	c1->Divide(2,1);
+	c1->cd(1);
+	TGraphErrors *g1 = new TGraphErrors(n, T, y1);
+	g1->SetTitle("Pression; r(km); #bar{p}");
+	g1->SetLineColor(kRed);
+	TAxis* a = g1->GetXaxis();
+    a->SetMaxDigits(5);
+	g1->Draw("al");
+	c1->cd(2);
+	TGraphErrors *g2 = new TGraphErrors(n, T, z1);
+	g2->SetTitle("Mass; r(km); #bar{#Mu}");
+	g2->SetLineColor(kBlue);
+	TAxis* b = g2->GetXaxis();
+    b->SetMaxDigits(5);
+	g2->Draw("al");
+}
+
+void neutronStarNonrelV1(){
+	double p0 = 1e-4; //Initial Pressure
+
+	//printf("p0\tR\t\tMass\n");
+	for (int i = 0; i < 1; ++i)
+	{
+		principal(p0);
+		p0 *= 1e-1;
+	}
+}
